22b0d8fd012e8146c2f687ba209eb4ba3d28968b
James Taranto <taranto.james@gmail.com>
2020-04-06 13:14:48 +1000

    test orderForm.ts

---
+++ src/public/js/orderForm.ts
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Place JavaScript code here...
+    $('.line-item').change(() => {
+        var element = this
+        console.log($(this));
+        console.log(produce);
+    })
+});
+
