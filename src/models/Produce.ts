fb97f57d2ccb7c4bc691e8404d68dbbbf318f23d
James Taranto <taranto.james@gmail.com>
2020-04-06 13:14:39 +1000

    Add complex pricing for produce

--- src/models/Produce.ts
+++ src/models/Produce.ts
@@ -3,21 +3,29 @@ import mongoose from "mongoose";
 export type ProduceDocument = mongoose.Document & {
     name: string;
     description: string;
-    price: number;
-    priceUnit: string;
-    units: [string];
+    priceOptions: [{
+        price: number;
+        unit: string;
+        min: number;
+        max: number;
+        step: number;
+    }];
     validFrom: Date;
     validTo: Date;
 };
 
 export const produceSchema = new mongoose.Schema({
     name: String,
-    price: Number,
     description: String,
-    priceUnit: String,
-    units: [String],
+    priceOptions: [{
+        price: Number,
+        unit: String,
+        min: Number,
+        max: Number,
+        step: Number
+    }],
     validFrom: Date,
     validTo: Date
 }, { timestamps: true });
 
-export const Produce = mongoose.model<ProduceDocument>("produce", produceSchema);
+export const Produce = mongoose.model<ProduceDocument>("Produce", produceSchema);
